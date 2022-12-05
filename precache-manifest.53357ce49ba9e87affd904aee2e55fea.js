687528741da85c7b759a6d6996390bae93a225f3
OGBO <hedosphere@gmail.com>
2022-12-05 12:53:15 +0100

    Updates

---
+++ precache-manifest.53357ce49ba9e87affd904aee2e55fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebe966e45f082b36d2d5",
+    "url": "/RoboSpace/static/js/main.ebe966e4.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/RoboSpace/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "4cc797df08474d59606d",
+    "url": "/RoboSpace/static/css/2.ccaf4f1e.chunk.css"
+  },
+  {
+    "revision": "4cc797df08474d59606d",
+    "url": "/RoboSpace/static/js/2.4cc797df.chunk.js"
+  },
+  {
+    "revision": "6166a7a17794ad955e6c",
+    "url": "/RoboSpace/static/js/runtime~main.6166a7a1.js"
+  },
+  {
+    "revision": "196ba816706f56612a48e8f5db46ce5b",
+    "url": "/RoboSpace/index.html"
+  }
+];
